1c936c1018d3c3f22d16a8d902c9caf3fbb5a26b
Molecular Man <sunflowerdeath@gmail.com>
2019-08-25 23:16:27 +0300

    js: add WebWebsocket

---
+++ js/packages/common/src/WebWebSocket.js
@@ -0,0 +1,98 @@
+import url from 'url'
+
+import EventEmitter from './EventEmitter.js'
+
+const WebSocketState = {
+	CONNECTING: 0,
+	OPEN: 1,
+	CLOSING: 2,
+	CLOSED: 3
+}
+
+const WEBSOCKET_EVENTS = ['start', 'error', 'close', 'message']
+
+class WebWebSocket {
+	constructor(urlString) {
+		let parsedUrl
+		try {
+			parsedUrl = new url.parse(urlString)
+		} catch (e) {
+			throw new Error(`Invalid URL: '${urlString}'.`)
+		}
+
+		if (parsedUrl.protocol !== 'ws:') {
+			throw new Error("WebSocket URL must have 'ws' protocol.")
+		}
+
+		const host = parsedUrl.host
+		const port = parsedUrl.port === null ? '80' : parsedUrl.port
+
+		this.eventEmitter = new EventEmitter(WEBSOCKET_EVENTS)
+		this.webSocket = new WebSocket(host, port)
+		this.webSocket.addStartHandler(() => {
+			this.eventEmitter.dispatchEvent('start')
+		})
+		this.webSocket.addMessageHandler(event =>
+			this.eventEmitter.dispatchEvent('message', event)
+		)
+		this.webSocket.addErrorHandler(event =>
+			this.eventEmitter.dispatchEvent('error', event)
+		)
+		this.webSocket.addCloseHandler(() =>
+			this.eventEmitter.dispatchEvent('close')
+		)
+		Object.assign(this, WebSocketState)
+	}
+
+	send(value) {
+		this.webSocket.send(value)
+	}
+
+	close() {
+		this.nativeWebSocket.close()
+	}
+
+	get state() {
+		return this.nativeWebSocket.state
+	}
+
+	get onstart() {
+		this.eventEmitter.getEventProperty('start')
+	}
+	set onstart(value) {
+		this.eventEmitter.setEventProperty('start', value)
+	}
+
+	get onmessage() {
+		this.eventEmitter.getEventProperty('message')
+	}
+	set onmessage(value) {
+		this.eventEmitter.setEventProperty('message', value)
+	}
+
+	get onclose() {
+		this.eventEmitter.getEventProperty('close')
+	}
+	set onclose(value) {
+		this.eventEmitter.setEventProperty('close', value)
+	}
+
+	get onerror() {
+		this.eventEmitter.getEventProperty('error')
+	}
+	set onerror(value) {
+		this.eventEmitter.setEventProperty('error', value)
+	}
+
+	addEventListener(event, value) {
+		this.eventEmitter.addEventListener(event, value)
+	}
+
+	removeEventListener(event, value) {
+		this.eventEmitter.removeEventListener(event, value)
+	}
+}
+
+Object.assign(WebWebSocket, WebSocketState)
+
+export default WebWebSocket
--- js/packages/examples/src/ws.js
+++ js/packages/examples/src/ws.js
@@ -1,8 +1,18 @@
-let ws = new WebSocket()
-ws.addStartHandler(() => {
-	log('start')
-	ws.send('Hello, server')
-})
-ws.addCloseHandler(() => log('close'))
-ws.addErrorHandler(() => log('error'))
-ws.addMessageHandler(event => log('message: ' + event.data))
+// let ws = new WebSocket()
+// ws.addStartHandler(() => {
+    // log('start')
+    // ws.send('Hello, server')
+// })
+// ws.addCloseHandler(() => log('close'))
+// ws.addErrorHandler(() => log('error'))
+// ws.addMessageHandler(event => log('message: ' + event.data))
+
+import WebWebSocket from '@advk/common/src/WebWebSocket'
+const ws = new WebWebSocket('ws://192.168.1.1:1234')
+ws.onstart = () => {
+    log('start')
+    ws.send('Hello')
+}
+ws.onmessage = event => log('message: ' + event.data)
+ws.onerror = event => log('error')
+ws.onclose = () => log('close')
