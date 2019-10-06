67a016a7546f95f28d821760659d3c4b92f113f5
Molecular Man <sunflowerdeath@gmail.com>
2019-10-06 17:32:43 +0300

    js: use velocity in scrollable

--- js/packages/react-renderer/src/components/Scrollable.js
+++ js/packages/react-renderer/src/components/Scrollable.js
@@ -3,6 +3,7 @@ import Animated from 'animated'
 import HoverRecognizer from '@advk/common/src/gestures/HoverRecognizer.js'
 import DragRecognizer from '@advk/common/src/gestures/DragRecognizer.js'
 import MultiRecognizer from '@advk/common/src/gestures/MultiRecognizer.js'
+import VelocityTracker from '@advk/common/src/gestures/VelocityTracker.js'
 import { Responder, Scroll } from '../nativeComponents'
 import KeyCode from './KeyCode.js'
 
@@ -12,21 +13,20 @@ const KeyAction = {
 	REPEAT: 2
 }
 
-const useStateWithGetter = initialState => {
-	const [state, setState] = useState(initialState)
-	const ref = useRef(initialState)
-	useEffect(() => {
-		ref.current = state
-	})
-	return [state, setState, () => ref.current]
-}
+const KEYBOARD_SCROLL_DISTANCE = 10
 
 const springToEdge = ctx => {
-	Animated.spring(ctx.scrollTopValue, { toValue: 0, damping: 10 }).start()
+	Animated.spring(ctx.scrollTopValue, {
+		toValue: 0,
+		bounciness: 6,
+        // velocity is based on seconds instead of milliseconds
+		velocity: ctx.velocityTracker.getVelocity() * 1000
+	}).start()
 }
 
 const onDragEnd = (ctx, event) => {
-	if (ctx.scrollTopValue.__getValue() < 0) {
+	const scrollTop = ctx.scrollTopValue.__getValue()
+	if (scrollTop < 0) {
 		springToEdge(ctx)
 		return
 	}
@@ -39,20 +39,15 @@ const onDragEnd = (ctx, event) => {
 			ctx.scrollTopValue.removeListener(ctx.decayListener)
 		}
 	)
-	// while decaying - update the velocity
-	// when decay becomes < 0 - stop animation and run spring with current
-	// velocity
-	log('decay start')
 }
-
-const KEYBOARD_SCROLL_DISTANCE = 10
 
 const onKeyEvent = (ctx, event) => {
 	if (event.action === KeyAction.PRESS || event.action === KeyAction.REPEAT) {
 		if (event.key === KeyCode.UP || event.key == KeyCode.DOWN) {
-			const nextScrollTop =
+			let nextScrollTop =
 				ctx.scrollTopValue.__getValue() +
 				(event.key === KeyCode.UP ? 1 : -1) * KEYBOARD_SCROLL_DISTANCE
+			nextScrollTop = Math.max(nextScrollTop, 0)
 			Animated.spring(ctx.scrollTopValue, {
 				toValue: nextScrollTop,
 				overshootClamping: true
@@ -65,11 +60,13 @@ const Scrollable = ({ children }) => {
 	const ref = useRef()
 	const ctx = useRef({
 		initialScrollTop: 0,
-		scrollTopValue: new Animated.Value(0)
+		scrollTopValue: new Animated.Value(0),
+		velocityTracker: new VelocityTracker()
 	})
 	useEffect(() =>
 		ctx.current.scrollTopValue.addListener(({ value }) => {
 			ref.current.scrollTop = Math.round(value)
+			ctx.current.velocityTracker.addPoint(Date.now(), value)
 		})
 	)
 	const [recognizer] = useState(
